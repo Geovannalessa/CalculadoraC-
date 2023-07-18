d9c4f64e9f72edb0fd0a176d9a6a3f67565c19de
Geovanna Lessa <geovannalessa2@gmail.com>
2023-07-18 20:50:05 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois numeros q deseja calcular:");
+        }
+    }
+}
