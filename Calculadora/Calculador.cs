8dfcd1b77ca084d8121a997c05c2d372b54bb583
Geovanna Lessa <geovannalessa2@gmail.com>
2023-07-19 22:41:52 -0300

    * Criado a classe Calculador; * Métodos de somar, subtrair, multiplicar e dividir criados; * Leitura para indificar qual é o operador selecionado;

---
+++ Calculadora/Calculador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Calculador
+    {
+        public double num1;
+        public double num2;
+       
+        public double Somar(double numero1,double numero2)
+        {
+            this.num1 = numero1;
+            this.num2 = numero2;
+            double soma = numero1 + numero2;
+            return soma;
+        }
+        public double Subtrair(double numero1, double numero2)
+        {
+            this.num1 = numero1;
+            this.num2 = numero2;
+            double subtrair = numero1 - numero2;
+            return subtrair;
+        }
+        public double Multiplicar(double numero1, double numero2)
+        {
+            this.num1 = numero1;
+            this.num2 = numero2;
+            double multiplicar = numero1 * numero2;
+            return multiplicar;
+        }
+        public double Dividir(double numero1, double numero2)
+        {
+            this.num1 = numero1;
+            this.num2 = numero2;
+            double dividir = numero1 / numero2;
+            return dividir;
+        }
+    }
+}
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace Calculadora
+﻿using System.Runtime.Serialization;
+
+namespace Calculadora
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite dois numeros q deseja calcular:");
+            Calculador n1, n2, calculadora;
+            n1 = new Calculador();
+            n2 = new Calculador();
+            calculadora = new Calculador();
+            double resultado;
+
+            Console.WriteLine("                         Calculadora");
+            bool continuar = true;
+            while (continuar == true) { 
+                Console.WriteLine("Digite o número correspontende ao calculo que deseja:");
+                Console.WriteLine("__________________________________________________________");
+                Console.WriteLine("| 1- Soma | 2- Subtração | 3- Multiplicação | 4- Divisão |");
+                Console.WriteLine("----------------------------------------------------------");
+                int operador = int.Parse(Console.ReadLine());
+                if (operador > 4) {
+                    Console.WriteLine("Número indisponível!");
+                    break;
+                }
+
+                Console.WriteLine("Digite dois números q deseja calcular");
+                Console.Write("Primeiro número: ");
+                n1.num1 = double.Parse(Console.ReadLine());
+                Console.Write("Segundo número: ");
+                n2.num2 = double.Parse(Console.ReadLine());
+                 
+                switch (operador) {
+                    case 1:
+                        resultado = calculadora.Somar(n1.num1, n2.num2);
+                        Console.WriteLine( "Resultado: {0}", resultado );
+                        break;
+                    case 2:
+                        resultado = calculadora.Subtrair(n1.num1, n2.num2);
+                        Console.WriteLine("Resultado: {0}", resultado);
+                        break;
+                    case 3:
+                        resultado = calculadora.Multiplicar(n1.num1, n2.num2);
+                        Console.WriteLine("Resultado: {0}", resultado);
+                        break;
+                    case 4:
+                        resultado = calculadora.Dividir(n1.num1, n2.num2);
+                        Console.WriteLine("Resultado: {0}", resultado);
+                        break;
+                }
+
+                Console.WriteLine("Deseja repetir?");
+                string repetir = Console.ReadLine();
+                if (repetir == "sim" || repetir == "Sim")
+                {
+                    continuar = true;
+                }
+                else {
+                    continuar = false;
+                }
+
+            }
+
         }
     }
 }
